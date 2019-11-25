a629eb633b905c38111f0b53e8557796f3973b41
“Saima <rahman.saima92@gmail.com>
2019-11-24 22:39:20 -0500

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -2,6 +2,24 @@
 # We've done it for you here.
 require_relative '../config/environment.rb'
 
+#lifters
+
+fzee = Lifter.new("fzee", 30)
+rafi = Lifter.new("rafi", 10)
+
+#gym
+
+grand = Gym.new("grand")
+planet = Gym.new("planet")
+fit_club = Gym.new("fit_club")
+
+#membership
+# :cost, :lifter, :gym
+
+cost_1 = Membership.new(20, fzee, grand )
+cost_2 = Membership.new(25, fzee, planet)
+cost_3 = Membership.new(10, rafi, fit_club)
+
 # test code goes here
 
 binding.pry
